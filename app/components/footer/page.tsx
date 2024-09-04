2b6ed59a65d3183fa94f43398ba1fc30141967f4
Bryan Christy <bryanchristy2016@gmail.com>
2024-09-04 16:31:34 +0530

    INIT: Initial commit

---
+++ app/components/footer/page.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+        <p>Hi</p>
+        </>
+    )
+}
+
+export default Footer;
---
+++ app/components/navbar/page.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <>
+        <p>Hello</p>
+        </>
+    )
+}
+
+export default Navbar;
