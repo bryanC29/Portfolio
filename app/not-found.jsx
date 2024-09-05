feb14b84f674760028c1cb6262d19ae1e68981d2
Bryan Christy <bryanchristy2016@gmail.com>
2024-09-05 14:51:21 +0530

    BUILD: 404 Error Page

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="flex justify-center flex-col items-center md:h-[85vh] sm:h-[70vh] h-[65vh]">
+            <p className="text-center text-9xl">:404:</p>
+            <p className="text-center text-7xl">Not Found</p>
+        </div>
+    )
+}
+
+export default NotFound;
